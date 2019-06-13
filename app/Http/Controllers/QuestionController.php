6b839097412baf3dd54c325c4b0b2e142c273ec7
Marijan3062 <brailo.marijan@gmail.com>
2019-06-13 09:15:52 +0200

    commit to reflect changes in question controller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class QuestionController extends Controller
 {
     //
     public function index()
     {
-        return 'This is the list of questions';
+        //return 'This is the list of questions';
+        $questions = DB::table('questions')->get();
+        var_dump($questions);
     }
 
     public function show()
